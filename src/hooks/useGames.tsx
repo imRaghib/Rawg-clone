3bcae334eb7aadd6b3febd58c0b7f70dde1b3a65
Raghib <raghib108@gmail.com>
2023-10-16 12:23:54 +0500

    Show loading skeletons

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -23,12 +23,18 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then(({ data }) => setGames(data.results))
+      .then(({ data }) => {
+        setLoading(false);
+        setGames(data.results);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
@@ -37,7 +43,7 @@ const useGames = () => {
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -2,9 +2,6 @@
 
 const getCroppedImageUrl = (url:string) =>{
     const target = 'media/'
-    console.log( url.indexOf(target));
-    console.log(target.length);
-    
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
 };
