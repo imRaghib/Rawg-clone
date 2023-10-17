b686f27242a564bdfa085f3f2a2ab96217d37bfa
Raghib <raghib108@gmail.com>
2023-10-17 15:02:08 +0500

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,22 @@
+import { Text } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: { [key: number]: string } = {
+    3: "😒",
+    4: "👍",
+    5: "🎯",
+  };
+
+  return (
+    <Text fontSize="xxl" marginTop={1}>
+      {emojiMap[rating]}
+    </Text>
+  );
+};
+
+export default Emoji;
